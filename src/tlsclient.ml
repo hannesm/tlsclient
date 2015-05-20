d38fe4b723207686d09c47fa6c2bd3345601466d
Hannes Mehnert <hannes@mehnert.org>
2015-05-20 17:34:48 +0100

    proper layouted chain info (x509#naming), keysize

--- src/tlsclient.ml
+++ src/tlsclient.ml
@@ -16,19 +16,28 @@ let tls_info t =
   let version = Tls.Printer.tls_version_to_string epoch.protocol_version
   and cipher = Sexplib.Sexp.to_string_hum (Tls.Ciphersuite.sexp_of_ciphersuite epoch.ciphersuite)
   and `Hex master = Hex.of_cstruct epoch.master_secret
-  and certs = String.concat "\n" (List.map X509.to_string epoch.peer_certificate)
+  and certs = List.flatten (List.map (fun x ->
+      [ "subject=" ^ X509.distinguished_name_to_string (X509.subject x) ;
+        "issuer=" ^ X509.distinguished_name_to_string (X509.issuer x) ])
+      epoch.peer_certificate)
   and trust =
     match epoch.trust_anchor with
     | None -> "NONE"
-    | Some x -> X509.to_string x
+    | Some x -> X509.distinguished_name_to_string (X509.subject x)
+  and pubkeysize = string_of_int (match epoch.peer_certificate with
+      | [] -> 0
+      | x::_ -> match X509.cert_pubkey x with
+        | `RSA p -> Nocrypto.Rsa.pub_bits p
+        | _ -> 0)
   in
   String.concat "\n" (List.map (fun (k, v) ->
-      (indent k 18) ^ ": " ^ v)
-      [ ("protocol", version) ;
-        ("cipher", cipher) ;
-        ("master secret", master) ;
-        ("certificate chain", certs) ;
-        ("trust anchor", trust) ])
+      (indent k 8) ^ ": " ^ String.concat (indent "\n" 11) v)
+      [ ("protocol", [version]) ;
+        ("cipher", [cipher]) ;
+        ("master", [master]) ;
+        ("keysize", [pubkeysize]) ;
+        ("chain", certs) ;
+        ("anchor", [trust]) ])
 
 let rec read_write buf ic oc =
   catch (fun () ->
