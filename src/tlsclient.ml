03ea9b64490749c894ab0ccfd322a9bde5d1854b
Hannes Mehnert <hannes@mehnert.org>
2015-12-09 18:39:40 +0000

    provide cert- and key-fingerprint authenticators, fixes for tls 0.7.0

--- src/tlsclient.ml
+++ src/tlsclient.ml
@@ -17,7 +17,7 @@ let tls_info t =
   let version = Tls.Printer.tls_version_to_string epoch.Tls.Core.protocol_version
   and cipher = Sexplib.Sexp.to_string_hum (Tls.Ciphersuite.sexp_of_ciphersuite epoch.Tls.Core.ciphersuite)
   and `Hex master = hex epoch.Tls.Core.master_secret
-  and certs = List.flatten (List.map (fun x ->
+  and pp_certs certs = List.flatten (List.map (fun x ->
       let hash = `SHA256 in
       let `Hex cert_fp = hex X509.(fingerprint hash x)
       and `Hex key_fp  = hex X509.(key_fingerprint ~hash (public_key x)) in
@@ -25,20 +25,24 @@ let tls_info t =
         "issuer=" ^ X509.distinguished_name_to_string (X509.issuer x) ;
         "certificate sha256 fingerprint: " ^ cert_fp ;
         "public key sha256 fingerprint:  " ^ key_fp ^ "\n"
-      ])
-      epoch.Tls.Core.peer_certificate)
+      ]) certs)
   and trust =
     match epoch.Tls.Core.trust_anchor with
     | None -> "NONE"
     | Some x -> X509.distinguished_name_to_string (X509.subject x)
   and pubkeysize = string_of_int (match epoch.Tls.Core.peer_certificate with
-      | [] -> 0
-      | x::_ -> match X509.public_key x with
+      | None -> 0
+      | Some x -> match X509.public_key x with
         | `RSA p -> Nocrypto.Rsa.pub_bits p
         | _ -> 0)
   and server_time =
     let peer_random = epoch.Tls.Core.peer_random in
     Int32.to_string (Cstruct.BE.get_uint32 peer_random 0)
+  in
+  let cert = match epoch.Tls.Core.peer_certificate with None -> [] | Some x -> [x] in
+  let chain, certs = match epoch.Tls.Core.trust_anchor with
+    | None -> "received", epoch.Tls.Core.received_certificates
+    | Some _ -> "chain", epoch.Tls.Core.peer_certificate_chain
   in
   String.concat "\n" (List.map (fun (k, v) ->
       (indent k 9) ^ ": " ^ String.concat (indent "\n" 12) v)
@@ -47,7 +51,8 @@ let tls_info t =
         ("cipher", [cipher]) ;
         ("master", [master]) ;
         ("keysize", [pubkeysize]) ;
-        ("chain", certs) ;
+        (chain, pp_certs certs) ;
+        ("server", pp_certs cert) ;
         ("anchor", [trust]) ])
 
 let rec read_write buf ic oc =
@@ -62,20 +67,32 @@ let rec read_write buf ic oc =
     (fun _ -> return_unit)
 
 
-let client zero_io cas fingerprint starttls host port =
+let client zero_io cas cfingerprint pfingerprint starttls host port =
   begin match starttls with
   | Some "xmpp" | None -> ()
   | Some s -> failwith ("Invalid argument to --starttls: " ^ s)
   end ;
   Nocrypto_entropy_lwt.initialize () >>
-  X509_lwt.authenticator (match cas, fingerprint with
-      | None   , None ->
-          Printf.printf "WARNING: Unauthenticated TLS connection\n" ;
-          `No_authentication_I'M_STUPID
-      | Some _ , Some _ ->
-          failwith "Error; both --ca and --fingerprint were supplied, I can't handle both"
-      | None    , Some hex_fp -> `Hex_key_fingerprints (`SHA256 , [(host, hex_fp)])
-      | Some ca , None        -> `Ca_dir ca) >>= fun authenticator ->
+  (match cas, cfingerprint, pfingerprint with
+   | None, None, None ->
+      Printf.printf "WARNING: Unauthenticated TLS connection\n" ;
+      X509_lwt.authenticator `No_authentication_I'M_STUPID
+   | Some _ , Some _, _
+   | Some _ , _, Some _
+   | _ , Some _, Some _ ->
+      failwith "Error; multiple authentication methods were supplied, I cannot handle this"
+   | None, Some hex_fp, None ->
+      let time = Unix.gettimeofday () in
+      let fp =
+        Nocrypto.Uncommon.Cs.of_hex
+          (String.map (function ':' -> ' ' | x -> x) hex_fp)
+      in
+      let fingerprints = [(host, fp)]
+      and hash = `SHA256
+      in
+      Lwt.return (X509.Authenticator.server_cert_fingerprint ~time ~hash ~fingerprints)
+   | None, None, Some hex_fp -> X509_lwt.authenticator (`Hex_key_fingerprints (`SHA256 , [(host, hex_fp)]))
+   | Some ca, None, None -> X509_lwt.authenticator (`Ca_dir ca)) >>= fun authenticator ->
   catch (fun () ->
     Lwt_unix.gethostbyname host >>= fun host_entry ->
     let host_inet_addr = Array.get host_entry.Lwt_unix.h_addr_list 0 in
@@ -122,13 +139,13 @@ let client zero_io cas fingerprint starttls host port =
        Printf.printf "failed to establish TLS connection: %s\n" (Printexc.to_string exn) ;
        return_unit)
 
-let run_client zero_io cas fingerprint starttls (host, port) =
+let run_client zero_io cas cfingerprint pfingerprint starttls (host, port) =
   Printexc.register_printer (function
       | Tls_lwt.Tls_alert x -> Some ("TLS alert: " ^ Tls.Packet.alert_type_to_string x)
       | Tls_lwt.Tls_failure f -> Some ("TLS failure: " ^ Tls.Engine.string_of_failure f)
       | _ -> None) ;
   Sys.(set_signal sigpipe Signal_ignore) ;
-  Lwt_main.run (client zero_io cas fingerprint starttls host port)
+  Lwt_main.run (client zero_io cas cfingerprint pfingerprint starttls host port)
 
 open Cmdliner
 
@@ -160,10 +177,13 @@ let zero_io =
   let doc = "zero-I/O mode [terminate after printing session info]" in
   Arg.(value & flag & info ["z"; "zero-io"] ~doc)
 
-let fingerprint =
-  let doc = "Authenticate the host's public key (the signing key; NOT the certificate) "
-          ^ "using a user-supplied SHA256 fingerprint" in
-  Arg.(value & opt (some string) None & info ["fingerprint"] ~docv:"SHA256_HEX" ~doc)
+let cfingerprint =
+  let doc = "Authenticate the host's certificate using a user-supplied SHA256 fingerprint" in
+  Arg.(value & opt (some string) None & info ["cert-fingerprint"] ~docv:"SHA256_HEX" ~doc)
+
+let pfingerprint =
+  let doc = "Authenticate the host's public key using a user-supplied SHA256 fingerprint" in
+  Arg.(value & opt (some string) None & info ["key-fingerprint"] ~docv:"SHA256_HEX" ~doc)
 
 let starttls =
   let doc = "Initiate connection using STARTTLS. Currently supported protocols: [xmpp]" in
@@ -174,12 +194,13 @@ let cmd =
   let man = [
     `S "DESCRIPTION" ;
     `P "$(tname) connects to a server and initiates a TLS handshake" ;
+    `P "Information about the TLS session is printed, including trust anchor and chain of trust (if applicable), otherwise list of received certificates.";
     `S "BUGS" ;
     `P "Please report bugs on the issue tracker at <https://github.com/hannesm/tlsclient/issues>" ;
     `S "SEE ALSO" ;
     `P "$(b,s_client)(1)" ]
   in
-  Term.(pure run_client $ zero_io $ cas $ fingerprint $ starttls $ destination),
+  Term.(pure run_client $ zero_io $ cas $ cfingerprint $ pfingerprint $ starttls $ destination),
   Term.info "tlsclient" ~version:"0.1.0" ~doc ~man
 
 let () =
